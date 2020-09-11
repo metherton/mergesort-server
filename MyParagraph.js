7c4e3b2e42d2c39e65f70c364f44567383859468
metherton <martinetherton63@gmail.com>
2020-09-11 22:29:50 +0200

    Added readme

--- MyParagraph.js
+++ MyParagraph.js
@@ -2,11 +2,13 @@ export class MyParagraph extends HTMLElement {
     constructor() {
       super();
 
-      const template = document.getElementById('my-paragraph');
-      const templateContent = template.content;
+        this.innerHTML = `<style> p {color: red;}</style><p>My web component</p>`;
 
-      this.attachShadow({mode: 'open'}).appendChild(
-        templateContent.cloneNode(true)
-      );
+//      const template = document.getElementById('my-paragraph');
+//      const templateContent = template.content;
+//
+//      this.attachShadow({mode: 'open'}).appendChild(
+//        templateContent.cloneNode(true)
+//      );
     }
 }
